34d482e635d26aeb449ed4958f730465fe70c8f4
Roy Portas <royportas@gmail.com>
2020-09-20 09:42:32 +1000

    Moved base os to centos

--- data/bootstrap.sh
+++ data/bootstrap.sh
@@ -1,14 +1,19 @@
 # Boostraps a node to run k3s
 # Should be ran as root
-apt-get update
-apt-get install -y containerd
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --no-deploy traefik" sh
 
-# Kubernetes dashboard
-GITHUB_URL=https://github.com/kubernetes/dashboard/releases
-VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
-sudo k3s kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
-sudo k3s kubectl create -f /data/dashboard.admin-user.yml -f /data/dashboard.admin-user-role.yml
+# Install containerd
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf install -y containerd.io
+
+# Create this directory, the k3s installer will copy binaries to here
+sudo mkdir /usr/local/bin
+
+yum install -y container-selinux selinux-policy-base
+rpm -i https://rpm.rancher.io/k3s-selinux-0.1.1-rc1.el7.noarch.rpm
+
+# Don't enable Traefik, as its an older version
+# Install your preferred ingress
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --disable traefik" sh
 
 # Copy the kubectl config to the data directory, to share with the host
 cp /etc/rancher/k3s/k3s.yaml /data/config
